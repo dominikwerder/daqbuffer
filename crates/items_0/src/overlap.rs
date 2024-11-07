b913fcc8c640f8694d62c4bf233307f2614a20eb
Dominik Werder <dominik.werder@gmail.com>
2024-11-07 20:55:38 +0100

    factor out items-0

--- crates/items_0/src/overlap.rs
+++
@@ -1,7 +0,0 @@
-// TODO rename, no more deque involved
-pub trait HasTimestampDeque {
-    fn timestamp_min(&self) -> Option<u64>;
-    fn timestamp_max(&self) -> Option<u64>;
-    fn pulse_min(&self) -> Option<u64>;
-    fn pulse_max(&self) -> Option<u64>;
-}
